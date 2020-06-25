cfaf3d3e09df15f5fe3cd1a9c72561986ffed597
Adriano Mota <motacsadriano@gmail.com>
2020-06-24 22:54:17 -0300

    Initial Commit

--- src/main/java/br/com/rabbitmq/consumer/module/RabbitMqSpringBootConsumerModuleApplication.java
+++ src/main/java/br/com/rabbitmq/consumer/module/RabbitMqSpringBootConsumerModuleApplication.java
@@ -1,13 +1,20 @@
 package br.com.rabbitmq.consumer.module;
 
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RabbitMqSpringBootConsumerModuleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitMqSpringBootConsumerModuleApplication.class, args);
+	}
+	
+	@Bean
+	public Jackson2JsonMessageConverter converter() {
+		return new Jackson2JsonMessageConverter();
 	}
 
 }
